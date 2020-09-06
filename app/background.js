212545c334e01c87cf209985e1ad8fef47b892f4
adsy2010 <adsy2010@users.noreply.github.com>
2020-09-06 23:03:50 +0100

    Moved to easier to manage app directory

--- app/background.js
+++ app/background.js
@@ -1,23 +1,20 @@
-const os = require('os');
+const os = require('node.os');
+
+try{
+
+    // Printing user information
+    console.log(os.platform);
+} catch(err){
+
+    // Printing exception if any
+    console.log(": error occured" + err);
+}
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
 
         console.log("The color is green.");
-        try{
 
-            // Setting options for os.userInfo()
-            // method
-            var options = {
-                encoding: 'buffer'
-            };
-
-            // Printing user information
-            console.log(os.userInfo(options));
-        } catch(err){
-
-            // Printing exception if any
-            console.log(": error occured" + err);
-        }
     });
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
